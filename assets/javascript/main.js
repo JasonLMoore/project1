1b8a77339e2986ad2b8c1654cf3dabb4eca16cf7
Justin Klaas <klaas.justin@gmail.com>
2019-04-09 20:04:43 -0500

    added js to main

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,4 +9,19 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $('#spell-adder').modal();
+  $('#dice-roller').modal();
 });
+
+ $('#roll').on('click', function () {
+      var userInput = $('#user-input').val();
+      var result = droll.roll(userInput);
+      var resultDiv = $('<div>');
+      $('.results').html(resultDiv);
+      resultDiv.text(result.total);
+      $('#user-input').val("");
+      console.log(result);
+ });
+
+ $('#roll-dice').on('click', function () {
+    $('.results').val("")
+ })
