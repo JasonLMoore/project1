582aeb5a0ff00ed59721eebd16fc76dfbd773150
Jason Moore <jason.moore1369@gmail.com>
2019-04-09 20:26:29 -0500

    text bugs repaired

--- assets/javascript/run.js
+++ assets/javascript/run.js
@@ -1,5 +1,5 @@
 
-var searchWord = "Magic Missile" 
+var searchWord = "Vampiric Touch" 
 var proxyURL = "https://cors-anywhere.herokuapp.com/"
 var queryURL = proxyURL + "http://dnd5eapi.co/api/spells/?name=" + searchWord
 
@@ -14,25 +14,56 @@ $.ajax({
         method: "GET"
     }).then(function(response) {
         console.log(response)
-
+        
         $("#name").html("<h1>" + response.name + "</h1>");
+
         $("#level").text("Spell Level: " + response.level);
+        if (response.level < 1) {
+            $("#level").text("Spell Level: Cantrip");
+        }
+
         $("#school").text("School of Magic: " +response.school.name);
         //Make appear only when true//
-        $("#ritual");
+
+        $("#con").text("Concentration")
+        if (response.concentration !== "yes") {
+            $("#con").hide();
+        }
+
+        $("#ritual").text("Ritual");
+        if (response.ritual !== "yes") {
+            $("#ritual").hide();
+        }
+
+
         ///////////////////////
         $("#castingTime").text("Casting Time: " + response.casting_time);
-        $("#range").text("Spell Range: " + response.range);
-        //Maybe not a thing?//
-        $("#a-o-e");
-        ////////////////////
+
+        $("#range").text("Range: " + response.range);
+
         $("#componants").text("Componants: " + response.components);
-        //Bug:word needs to be recoded//
-        $("#material").text("Materials: " + response.material);
-        /////////////////////////
+
+        if (response.material === undefined) {
+            $("material").hide()
+        } else {
+            response.material = response.material.split('â€™').join('\''); 
+            $("#material").text("Materials: " + response.material);
+        }
+        
         $("#duration").text("Duration: " + response.duration);
-        $("#descript").text("Description: " + response.desc);
+
+        $("#descript").text("Description: ");
+        for (n = 0; n < response.desc.length; n++){
+            response.desc[n] = response.desc[n].split('â€™').join('\'');
+            $("#descript").append(response.desc[n] + " ");
+        }
+
+        
         $("#atHigherLevel").text("At Higher Levels: " + response.higher_level);
+        if (response.higher_level === undefined) {
+            $("#atHigherLevel").hide();
+        }
+
         $("#usedBy").text("Can be cast by: ")  
         for(var i = 0; i < response.classes.length; i++) {
             $("#usedBy").append(response.classes[i].name);
@@ -40,5 +71,11 @@ $.ajax({
                 $("#usedBy").append(", ");
             }
         }
+
     });
 });
+
+//itemDescription = itemDescription.split('â€™').join('\'');
+//fix apostrophie itemDescription = itemDescription.split(',-').join('').split('.,').join('.');
+
+
